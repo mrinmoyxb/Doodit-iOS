265a8f043a4fb663a6d42f03ac173f77cf280ad7
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-01 20:26:49 +0530

    Initial Commit

---
+++ Doodit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Doodit
+//
+//  Created by Mrinmoy Borah on 01/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Doodit/DooditApp.swift
@@ -0,0 +1,17 @@
+//
+//  DooditApp.swift
+//  Doodit
+//
+//  Created by Mrinmoy Borah on 01/05/24.
+//
+
+import SwiftUI
+
+@main
+struct DooditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
