0ecf309beaccfa419ca494626c611c5cb3a77cf0
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-01 21:23:52 +0530

    created list view for tasks

---
+++ Doodit/View/Components/ListRowView.swift
@@ -0,0 +1,8 @@
+//
+//  ListRowView.swift
+//  Doodit
+//
+//  Created by Mrinmoy Borah on 01/05/24.
+//
+
+import Foundation
---
+++ Doodit/View/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  Doodit
+//
+//  Created by Mrinmoy Borah on 01/05/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
