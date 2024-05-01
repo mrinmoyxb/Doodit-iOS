0b91c245eb1e30565326c544c928a6dc24d41db7
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-01 22:05:30 +0530

    textfield using swift UI

--- Doodit/View/AddView.swift
+++ Doodit/View/AddView.swift
@@ -8,11 +8,31 @@
 import SwiftUI
 
 struct AddView: View {
+    
+    @State var textInput: String = ""
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView{
+            VStack {
+                TextField("Write down your task...", text: $textInput)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(.taskbox)
+                    .cornerRadius(10)
+                
+                Button(action: {}, label: {Text("SAVE")
+                        .font(.headline)
+                        .bold()
+                        .frame(maxWidth: .infinity).frame(height: 55)
+                }).buttonStyle(.borderedProminent)
+                
+            }.padding(14)
+        }.navigationTitle("Add your task 🎯")
+
     }
 }
 
 #Preview {
-    AddView()
+    NavigationStack{
+        AddView()
+    }
 }
