86b70e3bdfd8336153f08e423afb8db3453bf7db
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-02 19:10:15 +0530

    alert and check characters length function

--- Doodit/View/AddView.swift
+++ Doodit/View/AddView.swift
@@ -12,6 +12,8 @@ struct AddView: View {
     @Environment(\.presentationMode) var presentationMode
     @EnvironmentObject var listViewModel: ListViewModel
     @State var textInput: String = ""
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView{
@@ -30,12 +32,31 @@ struct AddView: View {
                 
             }.padding(14)
         }.navigationTitle("Add your task 🎯")
+            .alert(isPresented: $showAlert, content: getAlert)
 
     }
     
+    // add text
     func saveTask(){
-        listViewModel.addItem(title: textInput)
-        presentationMode.wrappedValue.dismiss()
+        if textIsAppropriate(){
+            listViewModel.addItem(title: textInput)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    // check the length of text, must be > 3
+    func textIsAppropriate()-> Bool{
+        if textInput.count < 3{
+            alertTitle = "Your new task must be atleast 3 characters long!!! 🤓"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    // alert func
+    func getAlert()-> Alert{
+        return Alert(title: Text(alertTitle))
     }
 }
 
