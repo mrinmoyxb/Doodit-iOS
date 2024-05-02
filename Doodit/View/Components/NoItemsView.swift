80f53cf81649e737d2d14d5fef80208489c8e4e7
mrinmoyborah <mrinmoyborah26100@gmail.com>
2024-05-02 20:55:45 +0530

    created aimated add note button

---
+++ Doodit/View/Components/NoItemsView.swift
@@ -0,0 +1,18 @@
+//
+//  NoItemsView.swift
+//  Doodit
+//
+//  Created by Mrinmoy Borah on 02/05/24.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoItemsView()
+}
